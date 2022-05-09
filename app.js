3f5d4c385898c98376adc2c99b43fc349ca3097e
VinayakrishnaB <krishna.vinaya@gmail.com>
2022-05-09 17:25:31 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My new AWS CI/CD Pipelinewq'));
+app.get('/', (req, res) => res.send('Completed My new AWS CI/CD Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
